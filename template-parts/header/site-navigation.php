5b86607b3decbab0ce814063507701b814576155
Shramee Srivastav <shramee.srivastav@gmail.com>
2019-08-16 09:06:39 +0800

    WIP navigation

--- template-parts/header/site-navigation.php
+++ template-parts/header/site-navigation.php
@@ -10,14 +10,14 @@
 $main_menu = wp_nav_menu( [
 	'theme_location' => 'main-menu',
 	'container_class'     => 'main-navigation menu dn db-l',
-	'container_id'        => 'main-menu',
+	'container_id'        => 'main-nav',
 	'echo'           => false,
 ] );
 
 $mobile_menu = wp_nav_menu( [
 	'theme_location' => has_nav_menu( 'mobile-menu' ) ? 'mobile-menu' : 'main-menu',
 	'container_class'     => 'mobile-navigation menu absolute border-box dn right-0 w-100',
-	'container_id'        => 'mobile-menu',
+	'container_id'        => 'mobile-nav',
 	'echo'           => false,
 ] );
 
@@ -26,7 +26,9 @@ $mobile_menu = wp_nav_menu( [
 
 	<?php echo $main_menu; ?>
 
-	<a class="btn dn-l" data-toggle-slide=".mobile-navigation" href=""></a>
+	<a class="btn dn-l" data-toggle-slide="#mobile-nav" href="#mobile-nav">
+		<span class="caxton-hamburger flex flex-column"></span>
+	</a>
 	<?php echo $mobile_menu; ?>
 
 </nav>
